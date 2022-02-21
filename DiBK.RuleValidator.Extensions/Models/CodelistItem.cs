74121ece242115d097e0406d1f4166342bf64d8c
Tor Anders Gustavsen <tor.anders@arkitektum.no>
2022-02-21 14:56:11 +0100

    La til CodelistItem

---
+++ DiBK.RuleValidator.Extensions/Models/CodelistItem.cs
@@ -0,0 +1,16 @@
+﻿namespace DiBK.RuleValidator.Extensions
+{
+    public class CodelistItem
+    {
+        public string Name { get; private set; }
+        public string Value { get; private set; }
+        public string Description { get; private set; }
+
+        public CodelistItem(string name, string value, string description)
+        {
+            Name = name;
+            Value = value;
+            Description = description;
+        }
+    }
+}
